79591be630ef0ab18036b321c4b7c82f3b4869d3
Afra Al-Shabibi <a.shabibi20@gmail.com>
2024-10-23 10:10:34 +0400

    Add files via upload
    full part1 solution

--- SQL_Day3_Lab(part1).sql
+++ SQL_Day3_Lab(part1).sql
@@ -67,4 +67,73 @@ order by p.Dnum, e.Lname, e.Fname
 select salary from(select * ,ROW_NUMBER() over (order by salary desc)as rn from employee) as NewTable
 where rn in (1 , 2)
 						
+-------------------------------------------------------------------------------
+--9.Get the full name of employees that is similar to any dependent name
+select  fname from Employee
+where fname  in(select Dependent_name from Dependent where Fname like '%'+ Dependent_name+'%')
+-------------------------------------------------------------------------------
+--10.Display the employee number and name if at least one of them have dependents 
+--(use exists keyword) self-search.
+select ssn As Emplyee_Number, Fname As Name 
+from Employee where exists --This checks if there are any rows in the Dependents table that correspond to the current employee.
+				( select 1 from Dependent 
+					where SSN = ESSN)
+-------------------------------------------------------------------------------
+--11.In the department table insert new department called "DEPT IT" , 
+--with id 100, employee with SSN = 112233 as a manager for this department.
+--The start date for this manager is '1-11-2006'
+insert into Departments values ('DEPT IT',100,112233,'1-11-2006')
 
+--------------------------------------------------------------------------------
+--12.Do what is required if you know that : Mrs.Noha Mohamed(SSN=968574) 
+--moved to be the manager of the new department (id = 100), 
+--and they give you(your SSN =102672) her position (Dept. 20 manager) 
+
+-->First try to update her record in the department table
+
+update Departments
+set  MGRSSN = null
+where Dnum = 20
+
+update Departments
+set  MGRSSN = 968574
+where Dnum = 100
+
+-->Update your record to be department 20 manager.
+update Departments
+set  MGRSSN = 102672
+where Dnum = 20
+
+-->Update the data of employee number=102660 to be in your teamwork
+--(he will be supervised by you) (your SSN =102672)
+update Employee
+set  Superssn = 102672
+where SSN = 102660 					
+
+--------------------------------------------------------------------------------
+--13. Unfortunately the company ended the contract with Mr. Kamel Mohamed (SSN=223344) 
+--so try to delete his data from your database
+--in case you know that you will be temporarily in his position.
+update Employee
+set  Superssn = 102672
+where Superssn = 223344 
+
+update Departments
+set  MGRSSN = 102672
+where MGRSSN = 223344
+
+update Works_for
+set  ESSn = 102672
+where ESSn = 223344
+
+delete from Employee
+where SSN = 223344
+
+--------------------------------------------------------------------------------
+--14.Try to update all salaries of employees who work in Project ‘Al Rabwah’ by 30%
+
+update Employee
+set Salary = (Salary+ Salary * 0.3)
+where SSN in(  select w.ESSn from Works_for w 
+			where w.Pno in (select p.Pnumber from Project p where
+			p.Pname ='Al Rabwah'))
